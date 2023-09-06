057a66efd92f5bc78201c02482b7b6c32c49159f
invitro <invitrocuentas@gmail.com>
2023-09-06 16:55:31 -0500

    Cambios loaders de paginas y validaciones de acceso

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,68 +1,73 @@
 import { postInicio, postVerify } from "../../services/user.service";
 
 const homeInicio = async () => {
-    if(document.querySelector('#home')){        
-        try {
-            const identificador = localStorage.getItem('identificador');
-            let objDatos = localStorage.getItem('objDatos');
-            if(!objDatos){
-                const rsp = await postVerify({id_user: identificador});
-                if(!rsp.existe){
-                    window.location.href = 'index.html';
-                }else{
-                    objDatos = JSON.stringify(rsp.data[0]);
-                    localStorage.setItem('objDatos', objDatos);
-                }
-            }
 
-            const datosUser = JSON.parse(objDatos);
-
-            document.getElementById('monedasUsuario').textContent = datosUser.puntos;
-            document.getElementById('nombreAvatar').textContent = datosUser.avatar;
-            document.getElementById('nombreAvatar2').textContent = datosUser.avatar;
-            document.getElementById('nivelUsuario').textContent = datosUser.nivel == null || datosUser.nivel == 'null' ? 1 : datosUser.nivel;
-
-            // Obtener elementos HTML por su ID
-            const personajeCarga = document.getElementById("personajeCarga");
-            const estadoCarga = document.getElementById("estadoCarga");
-            const estadoPorcentaje = document.getElementById("estadoPorcentaje");
-
-            const gradients = [
-                'linear-gradient(to bottom, #77CFE7, #009DC8, #005F8E, #015989)',
-                'linear-gradient(to bottom, #E7DC77, #C8B400, #8E8500, #895C01)',
-                'linear-gradient(to bottom, #E77777, #C80000, #8E0000, #890101)'
-            ];
-        
-            const colorText = [
-                'texto-borde_estado_85',
-                'texto-borde_estado_50',
-                'texto-borde_estado_15'
-            ];
-
-            // Actualizar el texto del porcentaje
-            estadoPorcentaje.textContent = `Estado: ${datosUser.barra_estado}%`;
-
-            // Actualizar el tamaño de la barra de carga
-            personajeCarga.style.left = `${datosUser.barra_estado - 8}%`;
-            estadoCarga.style.clipPath  = `inset(0 ${100 - datosUser.barra_estado}% 0 0)`;
-
-            if(datosUser.barra_estado >= 75){
-                estadoPorcentaje.classList.add(colorText[0]);
-                estadoCarga.style.backgroundImage = gradients[0];
-            }else if(datosUser.barra_estado >= 50 && datosUser.barra_estado < 75){
-                estadoPorcentaje.classList.add(colorText[1]);
-                estadoCarga.style.backgroundImage = gradients[1];
+           
+    try {
+        const identificador = localStorage.getItem('identificador');
+        let objDatos = localStorage.getItem('objDatos');
+        if(!objDatos){
+            const rsp = await postVerify({id_user: identificador});
+            if(!rsp.existe){
+                window.location.href = 'index.html';
             }else{
-                estadoPorcentaje.classList.add(colorText[2]);
-                estadoCarga.style.backgroundImage = gradients[2];
+                objDatos = JSON.stringify(rsp.data[0]);
+                localStorage.setItem('objDatos', objDatos);
             }
-
-        } catch (error) {
-            console.log(error)
-        } finally{
-            document.querySelector('#loader').classList.add('hidden');
         }
-    
+
+        const datosUser = JSON.parse(objDatos);
+
+        if(document.querySelector('#monedasUsuario')){
+            document.getElementById('monedasUsuario').textContent = datosUser.puntos;
+        }
+        if(document.querySelector('#nombreAvatar')){
+            document.getElementById('nombreAvatar').textContent = datosUser.avatar;
+        }
+        if(document.querySelector('#nombreAvatar2')){
+            document.getElementById('nombreAvatar2').textContent = datosUser.avatar;
+        }
+        if(document.querySelector('#nivelUsuario')){
+            document.getElementById('nivelUsuario').textContent = datosUser.nivel == null || datosUser.nivel == 'null' ? 1 : datosUser.nivel;
+        }
+        
+        if(document.getElementById("estadoPorcentaje")){
+                // Obtener elementos HTML por su ID
+                const personajeCarga = document.getElementById("personajeCarga");
+                const estadoCarga = document.getElementById("estadoCarga");
+                const estadoPorcentaje = document.getElementById("estadoPorcentaje");
+                const gradients = [
+                    'linear-gradient(to bottom, #77CFE7, #009DC8, #005F8E, #015989)',
+                    'linear-gradient(to bottom, #E7DC77, #C8B400, #8E8500, #895C01)',
+                    'linear-gradient(to bottom, #E77777, #C80000, #8E0000, #890101)'
+                ];
+
+                const colorText = [
+                    'texto-borde_estado_85',
+                    'texto-borde_estado_50',
+                    'texto-borde_estado_15'
+                ];
+                // Actualizar el texto del porcentaje
+                estadoPorcentaje.textContent = `Estado: ${datosUser.barra_estado}%`;
+                // Actualizar el tamaño de la barra de carga
+                personajeCarga.style.left = `${datosUser.barra_estado - 8}%`;
+                estadoCarga.style.clipPath  = `inset(0 ${100 - datosUser.barra_estado}% 0 0)`;
+                if(datosUser.barra_estado >= 75){
+                    estadoPorcentaje.classList.add(colorText[0]);
+                    estadoCarga.style.backgroundImage = gradients[0];
+                }else if(datosUser.barra_estado >= 50 && datosUser.barra_estado < 75){
+                    estadoPorcentaje.classList.add(colorText[1]);
+                    estadoCarga.style.backgroundImage = gradients[1];
+                }else{
+                    estadoPorcentaje.classList.add(colorText[2]);
+                    estadoCarga.style.backgroundImage = gradients[2];
+                }
+        }
+        
+    } catch (error) {
+        console.log(error)
+    } finally{
+        document.querySelector('#loader').classList.add('hidden');
     }
 };
 
--- src/js/pages/tienda.js
+++ src/js/pages/tienda.js
@@ -5,12 +5,12 @@ const tiendaOpciones = async () => {
     if(document.querySelector("#modulos-tienda")){
         try {
             let box = document.querySelector("#modulos-tienda")
-            let loading = false;
             const rsp = await getTienda();
             rsp.tienda.forEach(modulo => {
                 const {id, nombre, slug, imagen} = modulo
                 const boxModule = document.createElement("A")
                 boxModule.classList.add("flex", "flex-col", "justify-around", "items-center", "bg-white", "rounded-lg", "h-[35vh]", "w-[45vh]", "shadow-lg", "hover:cursor-pointer")
+                boxModule.href = `tiendaProductos.html`;
                 boxModule.innerHTML = `
                     <div class="h-[20vh] flex justify-center items-center">
                         <img class="hover:scale-110 duration-500 ease-in-out w-[14vh]" src="../../assets/tienda/${imagen}.png" alt="">
@@ -18,12 +18,7 @@ const tiendaOpciones = async () => {
                     <span class="text-[4vh] font-boldenvan text-[#009DC8]">${nombre}</span>            
                 `
                 box.appendChild(boxModule)
-                loading = true;
             });
-
-            if(loading){
-                document.querySelector('#loader').classList.add('hidden');
-            }
         } catch (error) {
             console.log(error)
         }
